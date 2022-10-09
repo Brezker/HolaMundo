146c79c42ccd1c94a1ec31d676aa0d7b5280aeb6
brezker <brezker10@gmail.com>
2022-10-09 15:24:14 -0500

    First commit

---
+++ ConexionBD.java
@@ -0,0 +1,32 @@
+// import java.util.Scanner;
+import java.sql.*;
+public class ConexionBD {
+  Connection conn = null;
+  public ConexionBD() {
+    String url = "jdbc:mysql://localhost:3306/saludos";
+    String user = "root", password = "root";
+    try {
+      conn = DriverManager.getConnection(url,user,password);
+    } catch (SQLException e) {
+      System.out.print("Error! " + e.getMessage());
+    }
+  }
+  public Connection getConnection() {
+    return this.conn;
+  }
+  public void desconectarBD() {
+    if (conn != null) {
+      try {
+        conn.close();
+      } catch (SQLException e) {
+        System.out.print("Error de desconexion! " + e.getMessage());
+      }
+    }
+  }
+    /*
+    Scanner leer = new Scanner(System.in);
+    System.out.print("Escribe tu saludo: ");
+    String msg = leer.nextLine();
+    System.out.print("--> " + msg);
+    */
+}
---
+++ Hola.java
@@ -0,0 +1,7 @@
+public class Hola{
+  public static void main(String asdf[]) {
+    System.out.println("Mostrar el Hola Mundo");
+    Select obj = new Select();
+    obj.mostrar();
+  }
+}
