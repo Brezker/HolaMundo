010d31f08a793f51b67a4c103d6050eddc9786f4
brezker <brezker10@gmail.com>
2022-10-10 23:53:52 -0500

    Add Insert

---
+++ Insert.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Insert{
+  Scanner leer = new Scanner(System.in);
+  String msg = null;
+  public void insertar(){
+    try{
+      ConexionBD bd = new ConexionBD();
+      ConexionBD conn = bd.getConnection();
+      Statement sentencia = conn.createStatement();
+      msg = leer.nextLine();
+      sentencia.executeUpdate("INSERT INTO mensaje VALUES(null, '"+ msg +"')");
+      conn.close();
+    } catch(SQLException e){
+      System.out.print("Error! "+ e.getMessage());
+    }
+  }
+}
